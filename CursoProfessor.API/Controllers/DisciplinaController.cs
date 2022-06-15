f7a112cea101208710dc42db3d3a2b5c9f3674c3
Maikel Ribeiro <tyrannosaurusrex@hotmail.com>
2022-06-15 13:39:41 -0300

    Adicionando DisciplinaController perdida no git de outro PC

---
+++ CursoProfessor.API/Controllers/DisciplinaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CursoProfessor.API.Controllers
+{
+    public class DisciplinaController : Controller
+    {
+        public IActionResult Inicio()
+        {
+            return Ok("Teste - Disciplina");
+        }
+    }
+}
